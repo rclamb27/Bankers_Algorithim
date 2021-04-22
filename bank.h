6666455f1cf95287e4f1d58ab10b19f1d0475ec8
rclamb27 <61068406+rclamb27@users.noreply.github.com>
2021-04-22 16:48:22 -0700

    Uploading File

---
+++ bank.h
@@ -0,0 +1,86 @@
+//
+//  bank.h
+//
+//
+
+#ifndef __BANK_H__
+#define __BANK_H__
+
+#include "customer.h"
+
+
+class Bank {
+public:
+  Bank() = default;
+  Bank(const ext_vector<int>& available) : avail(available), customers() { }
+  
+  ext_vector<int> get_avail() const { return avail; }
+  bool is_avail(const ext_vector<int>& req) const { return req < avail; }
+  
+  // TODO: determine if alloc is safe
+  bool is_safe(int id, const ext_vector<int>& req){ 
+    
+    bool output = false;
+    ext_vector<int> copy = this->avail;
+    copy -= req;
+
+    for(size_t i = 0; i < this->avail.size(); ++i){
+      if(i == id) continue; 
+      if(copy >= this->customers[i]->create_req())
+      {
+         output = true;
+      }
+    }
+    return output;
+  }   
+
+  bool req_approved(int id, const ext_vector<int>& req) {
+    if (req > avail) { return false; }
+
+    const Customer* c = customers[id];
+    if (c->needs_exceeded(req)) { return false; }
+
+    return is_safe(id, req);
+  }
+  
+  void add_customer(Customer* c) {
+    customers.push_back(c);
+    avail -= c->allocated();
+  }
+  
+  void withdraw_resources(const ext_vector<int>& req) {
+    if (!is_avail(req)) {
+      pthread_mutex_lock(&mutex_);
+      std::cerr << "WARNING: req: " << req << " is not available for withdrawing\n";
+      pthread_mutex_unlock(&mutex_);
+      return;
+    }
+    if (is_avail(req)) { avail -= req; }
+  }
+  void deposit_resources(const ext_vector<int>& req) { avail += req; }
+
+
+  ext_vector<Customer*> get_customers() const { return customers; }
+  
+  void show() const {
+    pthread_mutex_lock(&mutex_);
+    std::cout << "avail: [" << avail << "]\n";
+    pthread_mutex_unlock(&mutex_);
+    
+    for (Customer* c : customers) {
+      c->show();
+    }
+    std::cout << "\n";
+  }
+  
+  friend std::ostream& operator<<(std::ostream& os, const Bank& be) {
+    be.show();
+    return os;
+  }
+
+private:
+  ext_vector<int> avail;
+  ext_vector<Customer*> customers;
+};
+
+#endif /* Bank_h */
